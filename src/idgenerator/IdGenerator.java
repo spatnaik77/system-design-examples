5966aee79c6387ec86f1fe79482666ee40b1197c
spatnaik77 <spatnaik77@gmail.com>
2022-01-09 17:55:15 +0530

    distributed-id-generator

---
+++ src/idgenerator/IdGenerator.java
@@ -0,0 +1,7 @@
+package idgenerator;
+
+public class IdGenerator {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/idgenerator/Test.java
@@ -0,0 +1,9 @@
+package idgenerator;
+
+public class Test
+{
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+    }
+}
