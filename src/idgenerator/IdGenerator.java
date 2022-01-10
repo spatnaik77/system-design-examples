56b30b01958cf1b82a2fe72fba312e05d74f34c2
spatnaik77 <spatnaik77@gmail.com>
2022-01-10 20:55:54 +0530

    Distributed ID Generator

--- src/idgenerator/IdGenerator.java
+++ src/idgenerator/IdGenerator.java
@@ -1,7 +1,89 @@
 package idgenerator;
 
-public class IdGenerator {
-    public static void main(String[] args) {
+import java.time.Instant;
 
+//https://instagram-engineering.com/sharding-ids-at-instagram-1cf5a71e5a5c
+public class IdGenerator
+{
+     //Lock for sequence and lastTimestamp.
+     private final Object lock = new Object();
+
+    private static final int TOTAL_BITS = 64;
+    private static final int EPOCH_BITS = 41;
+    private static final int NODE_ID_BITS = 10;
+    private static final int SEQUENCE_BITS = 13;  //can generate 1023 ids per milli second!
+
+    private static final long maxNodeId   = (1L << NODE_ID_BITS) - 1;     // Maximum nodes = 2 ^ 13 -1 =
+    private static final long maxSequence = (1L << SEQUENCE_BITS) - 1;  // Maximum sequence = 2 ^ 10 - 1 = 1023
+
+    //Custom epoch starts from 01, Jan 2022
+    private static final long DEFAULT_CUSTOM_EPOCH = 1640975400000L;
+
+    private long nodeId;
+    private long customEpoch;
+
+    private volatile long lastTimestamp = -1L;
+    private volatile long sequence = 0L;
+
+    public IdGenerator(long nodeId)
+    {
+        if(nodeId < 0 || nodeId > maxNodeId)
+        {
+            throw new IllegalArgumentException(String.format("NodeId must be between %d and %d", 0, maxNodeId));
+        }
+        this.nodeId = nodeId;
+        this.customEpoch = DEFAULT_CUSTOM_EPOCH;
     }
+    public synchronized long nextId()
+    {
+        long currentTimestamp = timestamp();
+
+        //synchronized (lock)
+        //{
+            if(currentTimestamp < lastTimestamp)
+            {
+                throw new IllegalStateException("Invalid System Clock!");
+            }
+            if(currentTimestamp == lastTimestamp)
+            {
+                //Same milli second time slot. Increment sequence
+                if(sequence >= maxSequence)
+                {
+                    //throw new IllegalStateException("Too many calls. Slow down ! " + sequence);
+                    waitNextMillis();
+                }
+                ++sequence;
+            }
+            else
+            {
+                sequence = 0; //Reset sequence
+                lastTimestamp = currentTimestamp;
+            }
+            //Generate the id
+            long id = currentTimestamp << (NODE_ID_BITS + SEQUENCE_BITS)
+                    | (nodeId << SEQUENCE_BITS)
+                    | sequence;
+
+            return id;
+        //}
+    }
+    // Get current timestamp in milliseconds, adjust for the custom epoch.
+    private long timestamp()
+    {
+        return Instant.now().toEpochMilli() - customEpoch;
+    }
+    // Block and wait till next millisecond
+    private void waitNextMillis()
+    {
+        try
+        {
+            Thread.sleep(1);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- src/idgenerator/Test.java
+++ src/idgenerator/Test.java
@@ -1,9 +1,65 @@
 package idgenerator;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Test
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception
+    {
+        singleThreadedTest();
 
-        System.out.println("Hello");
+
+    }
+    private static void singleThreadedTest()
+    {
+        long startTime = System.currentTimeMillis();
+        HashSet<Long> set = new HashSet<Long>();
+        long duplicateCount = 0;
+        long totalCount = 1000000;
+
+        set.add(1l);
+        IdGenerator generator = new IdGenerator(1);
+        for(int c =1; c <= totalCount; c++)
+        {
+            boolean result = set.add(generator.nextId());
+            if(!result)
+            {
+                duplicateCount++;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken: " + (endTime-startTime));
+        System.out.println("Total: " + totalCount + " Duplicate count: " + duplicateCount + " Count in set: " + set.size());
+    }
+
+}
+class Task implements Runnable
+{
+    private IdGenerator generator;
+    private HashSet<Long> set;
+    private HashSet<Long> duplicateSet;
+    CountDownLatch latch;
+    public Task(IdGenerator generator, CountDownLatch latch, HashSet<Long> set, HashSet<Long> duplicateSet)
+    {
+        this.generator = generator;
+        this.set = set;
+        this.duplicateSet = duplicateSet;
+        this.latch = latch;
+    }
+    public void run()
+    {
+        long id = generator.nextId();
+        boolean result = set.add(id);
+        if(!result)
+        {
+            System.out.println("--------Dddddd");
+            duplicateSet.add(id);
+        }
+        latch.countDown();
     }
 }
